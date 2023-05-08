00001d36e49ac8767d2287ace00f08761e87af3f
AlisaMenshikova0 <123499442+AlisaMenshikova0@users.noreply.github.com>
2023-05-08 19:35:19 +0200

    Add files via upload

---
+++ jsl1.js
@@ -0,0 +1,53 @@
+//Task1
+
+let cleanerSalary = 40000;
+let teacherSalary = 22000;
+
+console.log(cleanerSalary + teacherSalary);
+
+//task2
+
+let town = "Vladivostok";
+let n = 2000;
+
+console.log(town + " " + n);
+
+//task 3
+
+const textTime = 184;
+const likeTime = 254;
+const readTime = 137;
+const youTubeTime = 201;
+
+console.log(
+  "Mermaid spent" +
+    textTime +
+    likeTime +
+    readTime +
+    youTubeTime +
+    "minutes procrastinating"
+);
+
+//task 4
+
+Math.pow(2, 5);
+
+//task 5
+
+let message =
+  "Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!";
+console.log(message);
+
+//Task 6
+const greetings = "Привет";
+const name = "Кот";
+
+console.log(greetings + name);
+
+//Task 8
+
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(
+  fahrenheit + "градуса по Фаренгейту - это" + celsius + "градуса по Цельсию."
+);
